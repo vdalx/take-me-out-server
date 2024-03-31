a482c564eecc97957729c82e42cda70f10305f33
vdalx <114463776+vdalx@users.noreply.github.com>
2024-03-31 12:11:27 -0400

    Added initial content for homepage in seed data file

--- migrations/20240326031841_create_content_table.js
+++ migrations/20240326031841_create_content_table.js
@@ -5,13 +5,14 @@
 exports.up = function(knex) {
     return knex.schema.createTable('content', (table) => {
         table.increments('id').primary();
-        table.string('article_name').notNullable();
-        table.string('article_author').notNullable();
-        table.date('article_date').notNullable();
-        table.string('article_location');
-        table.string('article_likes').notNullable();
-        table.string('article_tags');
-        table.string('article_body', [1600]).notNullable();
+        table.string('post_name').notNullable();
+        table.string('post_author');
+        table.date('post_date').notNullable();
+        table.string('post_location');
+        table.string('post_likes').notNullable();
+        table.string('post_tags');
+        table.string('post_desc').notNullable();
+        table.string('post_body', [1600]).notNullable();
         table.timestamp('created').notNullable().defaultTo(knex.fn.now());
         table.timestamp('modified').notNullable().defaultTo(knex.fn.now());
     })
---
+++ seed_data/contentData.js
@@ -0,0 +1,68 @@
+module.exports = [
+    {
+        id: 1,
+        post_name: "Trending: Top-Selling Performances You Can't Miss",
+        post_author: "",
+        post_date: "2024-03-24",
+        post_location: "United Kingdom",
+        post_likes: 101,
+        post_tags: [],
+        post_desc: "Discover the hottest tickets in town! We've rounded up the top-selling performances you can't afford to miss. Dive into the trendiest entertainment now!",
+        post_body: ""
+    },
+    {
+        id: 2,
+        post_name: "Favorite Music Venues: Where the Magic Happens",
+        post_author: "",
+        post_date: "2024-03-24",
+        post_location: "United Kingdom",
+        post_likes: 38,
+        post_tags: [],
+        post_desc: "Explore the beloved music venues where unforgettable moments are made. From intimate clubs to iconic arenas, discover the magic of live music!",
+        post_body: ""
+    },
+    {
+        id: 3,
+        post_name: "Artist Spotlight: Stellar Live in Concert!",
+        post_author: "",
+        post_date: "2024-03-24",
+        post_location: "United Kingdom",
+        post_likes: 119,
+        post_tags: [],
+        post_desc: "Get ready to experience the electrifying performance of Stellar live in concert! Dive into their music journey and prepare for an unforgettable night.",
+        post_body: "Get ready to groove with Stellar! Hailing from a small town, this electro pop rock sensation infuses their music with unique influences, creating a captivating sound. With catchy hooks, infectious beats, and stellar vocals (pun intended), Stellar's performances are a high-energy spectacle not to be missed. From intimate venues to larger stages, Stellar's star is on the rise. Join their journey and experience the electrifying energy of their live concerts!"
+    },
+    {
+        id: 4,
+        post_name: "Trending: Top-Selling Performances You Can't Miss",
+        post_author: "",
+        post_date: "2024-03-24",
+        post_location: "Canada",
+        post_likes: 98,
+        post_tags: [],
+        post_desc: "Discover the hottest tickets in town! We've rounded up the top-selling performances you can't afford to miss. Dive into the trendiest entertainment now!",
+        post_body: ""
+    },
+    {
+        id: 5,
+        post_name: "Favorite Music Venues: Where the Magic Happens",
+        post_author: "",
+        post_date: "2024-03-24",
+        post_location: "Canada",
+        post_likes: 22,
+        post_tags: [],
+        post_desc: "Explore the beloved music venues where unforgettable moments are made. From intimate clubs to iconic arenas, discover the magic of live music!",
+        post_body: ""
+    },
+    {
+        id: 6,
+        post_name: "Band Spotlight: Neon Horizon Live in Concert!",
+        post_author: "",
+        post_date: "2024-03-24",
+        post_location: "",
+        post_likes: 19,
+        post_tags: [],
+        post_desc: "Prepare for an unforgettable night with Neon Horizon live in concert! Delve into their musical journey and get ready to be dazzled.",
+        post_body: "Experience the magic of Neon Horizon! Originating from a quaint town, this electrifying band fuses electro-pop and rock elements, crafting a spellbinding sonic landscape. With infectious melodies, pulsating rhythms, and dynamic vocals, Neon Horizon's performances are a mesmerizing spectacle. From intimate clubs to grand arenas, they captivate audiences with their energetic stage presence and raw talent. Join the journey and immerse yourself in the vibrant energy of Neon Horizon's live concerts!"
+    },
+]
